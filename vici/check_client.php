524c6d1551e9de0be8be5e572c39ba9ae7fbb6bd
BlackThursdays <18381499+BlackThursdays@users.noreply.github.com>
2019-07-01 03:59:37 +0630

    Update check_client.php

--- vici/check_client.php
+++ vici/check_client.php
@@ -1,41 +1,35 @@
 <?php
-
 /**
  * @author Oleg Bosyuk
  * @copyright 2016
- * Примеры запросов
- * 1. Создание звонка:
+ * Request examples
+ * 1. Making a call:
  * http://192.168.5.216/agc/suitecrm/check_client.php?method=create_call&phone=2301&group=www&user_name=admin
- * 2. Обновление звонка:
+ * 2. Call update:
  * http://192.168.5.216/agc/suitecrm/check_client.php?method=update_call&call_id=57cd6861354e4&user_name=admin
  */
-
-// Сервер SuiteCRM
+// Server SuiteCRM
 $db_server = '10.8.0.3';
 $db_user = 'remote_user';
 $db_password = '111111';
 $db_name = 'suitecrm_779';
 $log_file = 'log.txt';
-//$name_secgroup = 'Контакт-центр';
-$current_date = gmdate('Y-m-d H:i:s'); // текущая дата
-
-// Вывод ошибки на экран
+// $ name_secgroup = 'Contact Center';
+$ current_date  =  gmdate ( ' Ymd H: i: s ' ); // current date
+// Display error
 function print_error($query = '')
 {
     global $log_file;
-
     $error_text = date("Y-m-d H:i:s") . " - Ошибка запроса $query: " . mysql_error() . "\n";
     file_put_contents($log_file, $error_text, FILE_APPEND | LOCK_EX);
     //echo $error_text;
 }
-
-
 /**
- * Проверяем клиента по номеру телефона. Порядок проверки:
- * 1. Контактные лица
- * 2. Клиенты
- * 3. Предварительные контакты
- * @return array клиентов
+* Check customer by phone number. Verification procedure:
+ * 1. Contact persons
+ * 2. Customers
+ * 3. Preliminary contacts
+* @return array clients
  *
  */
 function check_by_phone($phone)
@@ -43,8 +37,7 @@ function check_by_phone($phone)
     if (preg_match('/[^0-9]/', $phone)) {
         $phone = preg_replace('/[^0-9]/', '', $phone);
     }
-
-    // Формируем телефон в двух форматах:
+    // We form the phone in two formats:
     // 375...
     // 80....
     //if (preg_match('/^80/', $phone)) {
@@ -55,8 +48,7 @@ function check_by_phone($phone)
     //}
     //print_error($phone_f1);
     //print_error($phone_f2);
-
-    // Проверяем контактные лица
+    // Checking contact persons
     $select_contacts = "
 SELECT id
 FROM contacts
@@ -64,23 +56,18 @@ WHERE regex_replace('[^0-9]', '', phone_work) = '$phone_f1' OR
       regex_replace('[^0-9]', '', phone_work) = '$phone_f2' OR
       regex_replace('[^0-9]', '', phone_mobile) = '$phone_f1' OR
       regex_replace('[^0-9]', '', phone_mobile) = '$phone_f2' ";
-
     //print_error($select_contacts);
     $query = mysql_query($select_contacts);
-
     $clients = array();
-
-    // Если есть клиенты - загоняем в массив $clients
+    // If there are clients, we drive them into the $ clients array
     if (mysql_num_rows($query) > 0) {
         while ($row = mysql_fetch_assoc($query)) {
             $row['module'] = 'Contacts';
             $clients[] = $row;
         }
-
         //print_r($clients);
         return $clients;
-
-    } // Если контактных лиц нет - проверяем по клиентам
+    } // If there are no contact persons - check for customers
     else {
         $select_accounts = "
 SELECT id
@@ -89,20 +76,15 @@ FROM accounts
 WHERE regex_replace('[^0-9]', '', phone_office) = '$phone_f1' OR
       regex_replace('[^0-9]', '', phone_office) = '$phone_f2' OR 
       phones_c LIKE '%$phone_f1%' OR  phones_c LIKE '%$phone_f2%' ";
-
         //print_error($select_accounts);
         $query = mysql_query($select_accounts);
-
-
         if (mysql_num_rows($query) > 0) {
             while ($row = mysql_fetch_assoc($query)) {
                 $row['module'] = 'Accounts';
                 $clients[] = $row;
             }
-
             //print_r($clients);
             return $clients;
-
         } else {
             $select_leads = "
 SELECT id
@@ -115,26 +97,21 @@ WHERE regex_replace('[^0-9]', '', phone_work) = '$phone_f1' OR
       phones_c LIKE '%$phone_f1%' OR  phones_c LIKE '%$phone_f2%' ";
             //print_error($select_leads);
             $query = mysql_query($select_leads);
-
             if (mysql_num_rows($query) > 0) {
                 while ($row = mysql_fetch_assoc($query)) {
                     $row['module'] = 'Leads';
                     $clients[] = $row;
                 }
-
                 //print_r($clients);
                 return $clients;
-
             } else {
                 return [];
             }
-
         }
     }
 }
-
 /**
- * Создаем звонок, прикрепленный к клиенту (лиду)
+ * Create a call attached to the client (lida)
  * @param $clients
  * @return string
  */
@@ -146,8 +123,6 @@ function create_call($client, $phone)
     //$calls_secgroup_id = uniqid();
     global $current_date;
     //global $name_secgroup;
-
-
     $insert_call = "
 INSERT INTO calls 
 (id, status, name, date_start, date_entered, created_by, assigned_user_id, direction, parent_type, parent_id)
@@ -155,26 +130,21 @@ VALUES ('$call_id', 'Held', 'Звонок #$phone', '$current_date', '$current_d
 (SELECT DISTINCT id FROM users WHERE user_name like \"$_REQUEST[user_name]\"),
 (SELECT DISTINCT id FROM users WHERE user_name like \"$_REQUEST[user_name]\"),
 'Inbound', '$client[module]', '$client[id]') ";
-
     $insert_call_cstm = "
 INSERT INTO calls_cstm (id_c, vici_phone_c, vici_call_group_c, vici_fronter_phone_c, vici_uid_c, vici_channel_c)
 VALUES ('$call_id', '$phone', '$_REQUEST[group]', '$_REQUEST[fronter_phone]','$_REQUEST[uid]', '$_REQUEST[channel]') ";
-
     mysql_query($insert_call) or die (print_error($insert_call));
     mysql_query($insert_call_cstm) or die (print_error($insert_call_cstm));
-
     if ($client['module'] == 'Contacts') {
         $insert_calls_contacts = "
 INSERT INTO calls_contacts (id, call_id, contact_id, date_modified)
 VALUES ('$call_client_id', '$call_id', '$client[id]', '$current_date') ";
         mysql_query($insert_calls_contacts) or die (print_error($insert_calls_contacts));
-
     } elseif ($client['module'] == 'Leads') {
         $insert_calls_leads = "
 INSERT INTO calls_leads (id, call_id, lead_id, date_modified)
 VALUES ('$call_client_id', '$call_id', '$client[id]', '$current_date') ";
         mysql_query($insert_calls_leads) or die (print_error($insert_calls_leads));
-
     }
 //    elseif ($client['module'] == 'Accounts') {
 //
@@ -184,32 +154,24 @@ VALUES ('$call_client_id', '$call_id', '$client[id]', '$current_date') ";
 //        mysql_query($insert_calls_leads) or die (print_r($insert_calls_leads));
 //
 //    }
-
     //$insert_calls_secgroup = "INSERT INTO securitygroups_records (id, securitygroup_id, record_id, module, date_modified, created_by, deleted) " .
     //    "VALUES ('$calls_secgroup_id', (SELECT id FROM securitygroups WHERE name = '$name_secgroup'), '$call_id', 'Calls', '$current_date', 2, 0)";
-
     //print_r($call_id);
 //    print_r($insert_call);
 //    print_r($insert_calls_leads);
 //    print_r($insert_calls_contacts);
-
     //mysql_query($insert_calls_secgroup) or die (print_r($insert_calls_secgroup));
-
     return $call_id;
 }
-
-
 /**
- * Обновляем длительность звонок по окончанию
+ * Update the duration of the call at the end
  *
  * @return bool
  */
 function update_call($call_id)
 {
     global $current_date;
-
     $update_calls = "UPDATE calls JOIN calls_cstm ON calls_cstm.id_c = calls.id SET date_end = '$current_date', date_modified = '$current_date' ";
-
 //    if ((isset($_REQUEST['duration']) && !empty($_REQUEST['duration']))) {
 //        $duration = date("H:i:s", mktime(0, 0, $_REQUEST['duration'], 0, 0, 0));
 //        $update_calls .= " , duration = '$duration' ";
@@ -224,47 +186,35 @@ function update_call($call_id)
         $recording = 'http://' . $_SERVER['HTTP_HOST'] . '/RECORDINGS/MP3/' . $_REQUEST['recording'] . '-all.mp3';
         $update_calls .= " , vici_call_record_c = '$recording' ";
     }
-
     $update_calls .= " WHERE id = '$call_id'";
-
     //print_r($update_calls);
-
     mysql_query($update_calls) or die (print_error($update_calls));
     return true;
-
 }
-
-
-//Подключение к БД
+// Connect to the database
 mysql_connect($db_server, $db_user, $db_password) or die (print_error("Ошибка подключения к БД"));
 mysql_select_db($db_name) or die (print_error("Ошибка выбора БД"));
 mysql_query("SET NAMES 'utf8'") or die (print_error('Ошибка утановки utf8'));
-
-
 if ($_REQUEST['method'] === 'create_call') {
-
     if (isset($_REQUEST['phone']) && !empty($_REQUEST['phone'])) {
         $clients = check_by_phone($_REQUEST['phone']);
     } else {
         $clients = [];
     }
-
-    // Если клиент 1 - создаем звонок для него и возвращаем id
+    // If client 1 - create a call for it and return id
     if ($clients && count($clients) == 1) {
         $call_id = create_call($clients[0], $_REQUEST['phone']);
         $return = array('type' => 'found_one', 'call_id' => $call_id);
     }
-    // Если клиентов больше 1 - выводим список клиентов. По нажатию на url создаем звонок у клиента
+    // If there are more clients than 1, display the list of clients. By clicking on the url create a call to the client
     //elseif (count($clients) > 1) {
     //    $url = '';
     //}
-    // Если клиентов нет - создаем лида и звонок для него
+    // If there are no clients, create a lead and a call for it
     else if (!$clients) {
         $call_id = create_call($clients, $_REQUEST['phone']);
         $return = array('type' => 'not_found', 'call_id' => $call_id);
     }
-
-
 } else if ($_REQUEST['method'] === 'update_call') {
     if (isset($_REQUEST['call_id']) && !empty($_REQUEST['call_id'])) {
         $return = update_call($_REQUEST['call_id']);
@@ -272,7 +222,6 @@ if ($_REQUEST['method'] === 'create_call') {
         $return = false;
     }
 }
-
 //print_r($return);
 mysql_close();
 print_r(json_encode($return));
